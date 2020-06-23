5c4d2a668ba74f204019cde29792a37fd049ac12
Tiffany Forkner <thetif@gmail.com>
2020-06-23 13:22:28 -0400

    initial

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    'tailwindcss',
+    'pstcss-flexbug-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false
+        }
+      }
+    ]
+  ]
+}
